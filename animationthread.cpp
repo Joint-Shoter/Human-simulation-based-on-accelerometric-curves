d08f8e5604cf7ddda2f45630a7eca9b2e48229ca
morionnn <morionnnnn@gmail.com>
2024-11-06 02:21:12 +0300

    AnimationThread

--- animationthread.cpp
+++ animationthread.cpp
@@ -5,15 +5,78 @@
 //перерасчет для для всех указанных датчиков новых координат
 //перемещение костей в соответсвии с новыми координатами
 //-----------------------------------
-AnimationThread::AnimationThread()
+AnimationThread::AnimationThread(QObject* parent)
+    : QObject(parent), timer(new QTimer(this)), numBones(0)
 {
-
+    connect(timer, &QTimer::timeout, this, &AnimationThread::readNextData);
 }
 
-void readFromFile(){
-
+AnimationThread::~AnimationThread()
+{
+    stop();
 }
 
-void changePosAnimation(){
+void AnimationThread::startReadFile(const QString &filePath, int timeout)
+{
+    if (dataFile.isOpen()) {
+        dataFile.close();
+    }
 
+    dataFile.setFileName(filePath);
+
+    if (!dataFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        emit readerError("Failed to open file: " + dataFile.errorString());
+        return;
+    }
+
+    bonePos.clear();
+    boneRotate.clear();
+
+    QTextStream in(&dataFile);
+    if (!in.atEnd()) {
+        in >> numBones;
+        bonePos.resize(numBones);
+        boneRotate.resize(numBones);
+    }
+
+    timer->start(timeout);
 }
+
+void AnimationThread::stop()
+{
+    timer->stop();
+    if (dataFile.isOpen()) {
+        dataFile.close();
+    }
+}
+
+void AnimationThread::readNextData()
+{
+    if (!dataFile.isOpen()) {
+        emit readerError("File not open for reading.");
+        return;
+    }
+
+    QTextStream in(&dataFile);
+
+    if (!in.atEnd()) {
+        for (int i = 0; i < numBones; ++i) {
+            float xPos, yPos, zPos;
+            float xRot, yRot, zRot;
+
+            if (!(in >> xPos >> yPos >> zPos >> xRot >> yRot >> zRot)) {
+                emit readerError("Failed to read data for bone " + QString::number(i));
+                return;
+            }
+
+            bonePos[i] = QVector3D(xPos, yPos, zPos);
+            boneRotate[i] = QVector3D(xRot, yRot, zRot);
+        }
+
+        emit refreshAnim(bonePos, boneRotate);
+    } else {
+        stop();
+        emit readerError("End of file reached.");
+    }
+}
+
--- animationthread.h
+++ animationthread.h
@@ -2,11 +2,38 @@
 #define ANIMATIONTHREAD_H
 
 #include <QObject>
+#include <QTimer>
+#include <QVector>
+#include <QVector3D>
+#include <QFile>
+#include <QTextStream>
+#include <QString>
 
-class AnimationThread
+class AnimationThread : public QObject
 {
+    Q_OBJECT
+
 public:
-    AnimationThread();
+    AnimationThread(QObject* parent = nullptr);
+    ~AnimationThread();
+
+signals:
+    void refreshAnim(QVector<QVector3D> bonePos, QVector<QVector3D> boneRotate);
+    void readerError(QString err);
+
+public slots:
+    void startReadFile(const QString &filePath, int timeout);
+    void stop();
+
+private slots:
+    void readNextData();
+
+private:
+    QTimer* timer;
+    QVector<QVector3D> bonePos;
+    QVector<QVector3D> boneRotate;
+    QFile dataFile;
+    int numBones;
 };
 
 #endif // ANIMATIONTHREAD_H
